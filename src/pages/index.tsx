c11ca05e3dc5665e0db2f0f56da5848ab30715c5
Muhammad Azka Latif <azkakentipjr@gmail.com>
2026-08-18 21:53:53 +0700

    feat: setup project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
       <PageHead title="Hello"/>
-      <Button color="primary">Hello</Button>
+      <div className="flex">
+      <Button color="secondary">Hello</Button>
+      <Button color="primary">Wok</Button>
+        
+      </div>
     </main>
   );
 }
