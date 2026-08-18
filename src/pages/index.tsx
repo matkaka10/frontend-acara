591723b7e5c0adf54953eabca3e1a5f85acee05b
Muhammad Azka Latif <azkakentipjr@gmail.com>
2026-08-18 21:51:27 +0700

    feat: setup project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "@nextui-org/react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-      <PageHead/>
+      <PageHead title="Hello"/>
       <Button color="primary">Hello</Button>
     </main>
   );
