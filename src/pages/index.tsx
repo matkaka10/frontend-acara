b89639295f0e0cdc6aedc02951cb7ac197db59d9
Muhammad Azka Latif <azkakentipjr@gmail.com>
2026-08-19 20:02:40 +0700

    gass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
               <th>Jurusan</th>
               <th>Jurusan</th>
               <th>Jurusan</th>
-           
+           </tr>
           </form>
         </div>
       <Button color="secondary">Hello</Button>
