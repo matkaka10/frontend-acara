968efd878ceaefecad114232030c725f8c843d92
Muhammad Azka Latif <azkakentipjr@gmail.com>
2026-08-20 20:15:34 +0700

    feat: auth layout

---
+++ src/pages/auth/register/index.tsx
@@ -0,0 +1,12 @@
+import Authlayout from "@/components/layout/AuthLayout";
+import Register from "@/components/views/Register";
+
+const RegisterPage = () => {
+    return (
+        <Authlayout title="Acara | Register">
+            <Register />
+        </Authlayout>
+    )
+}
+
+export default RegisterPage
