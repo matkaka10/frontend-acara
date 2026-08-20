b946d9b09bf02891761ee7412cc24d83cd523dfd
Muhammad Azka Latif <azkakentipjr@gmail.com>
2026-08-20 19:26:04 +0700

    feat: auth reg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,36 +9,7 @@ export default function Home() {
       <PageHead title="Hello"/>
       <div className="">
         <div>
-          <form action="" className="border-1">
-            <tr className="border">
-              <th>ID</th>
-              <th>Nama</th>
-              <th>NIM</th>
-              <th>Jurusan</th>
-              asd
-              <aside>da</aside>
-              <th>Jurusan</th>
-          <aside><aside>asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            asd
-            <aside><data value="a"></data></aside></aside></aside>
-           </tr>
-           <address><address>ad</address></address>
-          </form>
+          
         </div>
       <Button color="secondary">Hello</Button>
       </div>
