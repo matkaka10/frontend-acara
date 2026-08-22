3e7c36eb01c42e7e11a5e57b73ae9a5e1a2bd532
Muhammad Azka Latif <azkakentipjr@gmail.com>
2026-08-22 20:25:01 +0700

    feat: ui register

--- src/components/views/Register/Register.tsx
+++ src/components/views/Register/Register.tsx
@@ -48,6 +48,12 @@ const Register = ( ) => {
                     width={1024}
                     height={1024}
                 />
+                  <Image 
+                    src="/images/ilustrations/login.svg"
+                    alt="login"
+                    width={1024}
+                    height={1024}
+                />
             </div>
             <Card>
                 <CardBody>
